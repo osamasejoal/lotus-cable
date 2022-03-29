117de7a0c871336cc744dbd8892a16dd1c015b78
osamashaheb <65346178+osamasejoal@users.noreply.github.com>
2022-03-29 23:23:28 +0600

    title change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>StartUI - Premium Bootstrap 4 Admin Dashboard Template</title>
+	<title>Lotus Cable</title>
 
 	<link href="{{asset('backend/assets')}}/img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
 	<link href="{{asset('backend/assets')}}/img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
